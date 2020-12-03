e9d09f470360919c355a377944aca4fc577e5f29
Vladislav Ekhlakov <ekhlakovvlad@gmail.com>
2020-12-03 14:47:35 +0300

    Fixed errors and typos

--- ruby_basics.part_1/discriminant.rb
+++ ruby_basics.part_1/discriminant.rb
@@ -1,22 +1,23 @@
 # here a, b, c - coefficients in D = (b^2 – 4ac)
 
 print "Please, enter the first coefficient (a): "
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 
 print "Please, enter the first coefficient (b): "
-b = gets.chomp.to_i
+b = gets.chomp.to_f
 
 print "Please, enter the first coefficient (c): "
-c = gets.chomp.to_i
+c = gets.chomp.to_f
 
-discriminant = (b**2 - 4*a*c)
+discriminant = (b**2 - 4 * a * c)
 
 if discriminant > 0
-  first_answer = ((-1) * b + Math.sqrt(discriminant))/(2.0*a)
-  second_answer = ((-1) * b - Math.sqrt(discriminant))/(2.0*a)
+  discriminant_root = Math.sqrt(discriminant)
+  first_answer = ((-1) * b + discriminant_root) / (2 * a)
+  second_answer = ((-1) * b - discriminant_root) / (2 * a)
   puts "Discriminant: #{discriminant}, first_answer: #{first_answer}, second_answer: #{second_answer}"
 elsif discriminant == 0
-  answer = (-1) * (b/(2.0*a))
+  answer = (-1) * (b / (2 * a))
   puts "Discriminant: #{discriminant}, answer: #{answer}"
 elsif discriminant < 0
   puts "Discriminant is less than zero. There is no answers"
--- ruby_basics.part_1/triangle_area.rb
+++ ruby_basics.part_1/triangle_area.rb
@@ -1,5 +1,5 @@
 print "Please, enter the base of the triangle: "
-base = gets.chomp.to_i
+base = gets.chomp.to_f
 
 if base <= 0 
   puts "Base should be greater than zero"
@@ -7,14 +7,14 @@ if base <= 0
 end
 
 print "Please, enter the height of the triangle: "
-height = gets.chomp.to_i
+height = gets.chomp.to_f
 
 if height <= 0
   puts "Height should be greater than zero"
   exit
 end
 
-triangle_area = (base * height)/2.0
+triangle_area = (base * height) / 2
 
 puts "The area of triangle is #{triangle_area}"
 
